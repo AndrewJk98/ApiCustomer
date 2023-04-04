c639cdf4d6873c23335c0fa6e0d44c5994bbfdc1
Manuel Huaman <manuelhuaman1998@gmail.com>
2023-04-03 22:24:47 -0500

    First Commit

---
+++ src/main/java/pe/cibertec/ecommerce/ApiCustomer/service/CustomerService.java
@@ -0,0 +1,10 @@
+package pe.cibertec.ecommerce.ApiCustomer.service;
+
+import java.util.List;
+import pe.cibertec.ecommerce.ApiCustomer.entity.Customer;
+
+public interface CustomerService {
+    public List<Customer> findAll();
+    public Customer findById(long id);
+    
+}
---
+++ src/main/java/pe/cibertec/ecommerce/ApiCustomer/service/CustomerServiceImpl.java
@@ -0,0 +1,24 @@
+package pe.cibertec.ecommerce.ApiCustomer.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.cibertec.ecommerce.ApiCustomer.dao.CustomerRepository;
+import pe.cibertec.ecommerce.ApiCustomer.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public List<Customer> findAll() {
+        return (List<Customer>) customerRepository.findAll();
+    }
+
+    @Override
+    public Customer findById(long id) {
+        return customerRepository.findById(id).orElse(null);
+    }
+    
+}
